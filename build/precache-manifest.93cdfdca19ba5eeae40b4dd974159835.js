87a931c61024893eccfc585cbe6101627ffa3534
WENDEL <dsdprotocol@protonmail.com>
2020-12-27 21:17:38 -0300

    build
    build to test live via github pages.

---
+++ build/precache-manifest.93cdfdca19ba5eeae40b4dd974159835.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f689bf321716128039cfb34db62308",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ca23c2223a243d7fab4",
+    "url": "./static/css/3.f61733be.chunk.css"
+  },
+  {
+    "revision": "26ba850095e033d08f79",
+    "url": "./static/js/0.11eda7b2.chunk.js"
+  },
+  {
+    "revision": "95c2a6d9dc867031d9ba",
+    "url": "./static/js/10.6b9da2d4.chunk.js"
+  },
+  {
+    "revision": "3ca23c2223a243d7fab4",
+    "url": "./static/js/3.0b01ae3f.chunk.js"
+  },
+  {
+    "revision": "e3cd3bf5907781de0d5251112a08d539",
+    "url": "./static/js/3.0b01ae3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25ef8e67d87a6bd556b",
+    "url": "./static/js/4.83fad6db.chunk.js"
+  },
+  {
+    "revision": "e10ade8de9df116fb09e",
+    "url": "./static/js/5.9304c1b9.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "./static/js/5.9304c1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152de4eb9a86171908c6",
+    "url": "./static/js/6.a5dbe700.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/6.a5dbe700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a10bafc806070d49d7",
+    "url": "./static/js/7.92683d36.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/7.92683d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9665ecf3b3ebd07796e0",
+    "url": "./static/js/8.9da78228.chunk.js"
+  },
+  {
+    "revision": "469a7b895233f83a3ed3",
+    "url": "./static/js/9.7e019aa7.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "./static/js/9.7e019aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d62abc3b606e8b6e122",
+    "url": "./static/js/main.4de0f922.chunk.js"
+  },
+  {
+    "revision": "a7a00069b3744194b707",
+    "url": "./static/js/runtime-main.d3e0ec44.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "./static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "./static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "./static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "./static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "./static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,23 +1,23 @@
 export const UNI = {
-  addr: '0x88ff79eB2Bc5850F27315415da8685282C7610F9',
+  addr: '0x88ff79eB2Bc5850F27315415da8685282C7610F9', // Uniswap V2: ESD-USDC
   decimals: 18,
   symbol: 'UNI',
 };
 
 export const USDC = {
-  addr: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  addr: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USD Coin
   decimals: 6,
   symbol: 'USDC',
 };
 
 export const ESD = {
-  addr: '0x36F3FD68E7325a35EB768F1AedaAe9EA0689d723',
+  addr: '0x36F3FD68E7325a35EB768F1AedaAe9EA0689d723', // Empty Set Dollar: ESD Token
   decimals: 18,
   symbol: 'ESD',
 };
 
 export const ESDS = {
-  addr: '0x443D2f2755DB5942601fa062Cc248aAA153313D3',
+  addr: '0x443D2f2755DB5942601fa062Cc248aAA153313D3', // Root/Implementation
   decimals: 18,
   symbol: 'ESDS',
 };
